619b951cf479b2ccb6dca7705ecc302c450f9c4b
Dios Vo <vtmn1212@gmail.com>
2025-06-30 18:09:12 +0700

    #1: Initialize repository and application

---
+++ src/app/layout.tsx
@@ -0,0 +1,44 @@
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+
+import { AppSidebar } from '@/components/app-sidebar';
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+
+import './globals.css';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'React Core',
+  description: 'Learn React with a focus on core concepts and best practices.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.className} ${geistMono.variable} antialiased`}
+      >
+        <SidebarProvider>
+          <AppSidebar />
+          <main>
+            <SidebarTrigger />
+            <div className="p-2">{children}</div>
+          </main>
+        </SidebarProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,94 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import { ChevronDown, Palette, Shapes, Webhook } from 'lucide-react';
+
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from '@/components/ui/collapsible';
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+} from '@/components/ui/sidebar';
+
+const sidebarData = [
+  {
+    title: 'Learn',
+    icon: Shapes,
+    items: [],
+  },
+  {
+    title: 'Hooks',
+    icon: Webhook,
+    items: ['useBoolean'],
+  },
+  {
+    title: 'UI',
+    icon: Palette,
+    items: [],
+  },
+] as const;
+
+export function AppSidebar() {
+  const pathname = usePathname();
+
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarMenu>
+          {sidebarData.map((item) => (
+            <Collapsible
+              key={item.title}
+              defaultOpen={item.title === 'Hooks'}
+              className="group/collapsible"
+              disabled={!item.items.length}
+            >
+              <SidebarMenuItem>
+                <CollapsibleTrigger asChild>
+                  <SidebarMenuButton>
+                    <item.icon />
+                    <span>{item.title}</span>
+                    <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+                  </SidebarMenuButton>
+                </CollapsibleTrigger>
+                <CollapsibleContent>
+                  <SidebarMenuSub>
+                    {item.items.map((subItem) => {
+                      const subItemPath = `/${item.title.toLowerCase()}/${subItem
+                        .replace(/([a-z])([A-Z])/g, '$1-$2')
+                        .toLowerCase()}`;
+                      const isSubItemActive = pathname === subItemPath;
+
+                      return (
+                        <SidebarMenuSubItem key={subItem}>
+                          <SidebarMenuSubButton
+                            asChild
+                            isActive={isSubItemActive}
+                          >
+                            <Link href={subItemPath}>
+                              <span>{subItem}</span>
+                            </Link>
+                          </SidebarMenuSubButton>
+                        </SidebarMenuSubItem>
+                      );
+                    })}
+                  </SidebarMenuSub>
+                </CollapsibleContent>
+              </SidebarMenuItem>
+            </Collapsible>
+          ))}
+        </SidebarMenu>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
