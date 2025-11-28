a525c2210e2eb1c1bd9e3b05321894ee0f0242e3
Dios Vo <vtmn1212@gmail.com>
2025-11-28 21:43:01 +0700

    #49: Practice with `useCallback`

--- src/app/ui/others/use-callback/Memoization.tsx
+++ src/app/ui/others/use-callback/Memoization.tsx
@@ -1,21 +1,30 @@
 'use client';
 
-import { memo, useState } from 'react';
+import { memo, useCallback, useState } from 'react';
 
 import { Button } from '@/components/ui/button';
 
 // 1. memo() -> HOC / Avoid rerender when unneeded
 // 2. useCallback()
+// --- Reference types
+// --- Only works when used with "React.memo()"
 
 // Common (Purpose: reuse logic)
 // - Hooks
 // - HOC
 // - Render props
 
-function Content() {
-  console.log('Re-render'); // Enable without memo
+function Content({ onIncrease }: { onIncrease: () => void }) {
+  console.log('Re-render');
 
-  return <p>Content</p>;
+  return (
+    <>
+      <p>Content</p>
+      <Button onClick={onIncrease} className="my-2">
+        Increase
+      </Button>
+    </>
+  );
 }
 
 const MemoizedContent = memo(Content);
@@ -23,16 +32,17 @@ const MemoizedContent = memo(Content);
 export default function Memoization() {
   const [count, setCount] = useState<number>(0);
 
-  const handleIncrease = () => {
-    setCount(count + 1);
-  };
+  // 🚩 Without useCallback, "handleIncrease" is recreated on every render
+  // Arrow function in props -> new reference
+  const handleIncrease = useCallback(() => {
+    setCount((prev) => prev + 1);
+  }, []); // Empty dependency array -> return the previous reference
 
   return (
     <div>
       {/* When "count" changes, Content will be re-rendered without memo */}
-      <MemoizedContent />
-      <p className="text-red-500 my-2">{count}</p>
-      <Button onClick={handleIncrease}>Increase</Button>
+      <MemoizedContent onIncrease={handleIncrease} />
+      <p className="text-red-500">{count}</p>
     </div>
   );
 }
